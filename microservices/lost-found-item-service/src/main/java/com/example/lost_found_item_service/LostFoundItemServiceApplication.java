fe1d9b3127ac2299ef367b2f46f40d1211b92ada
andrew-emil <147343944+andrew-emil@users.noreply.github.com>
2025-05-03 19:54:59 +0300

    first commit

---
+++ microservices/lost-found-item-service/src/main/java/com/example/lost_found_item_service/LostFoundItemServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.lost_found_item_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostFoundItemServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LostFoundItemServiceApplication.class, args);
+	}
+
+}
---
+++ microservices/lost-found-item-service/src/test/java/com/example/lost_found_item_service/LostFoundItemServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lost_found_item_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostFoundItemServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
