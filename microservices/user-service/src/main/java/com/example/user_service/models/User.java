197c887d2349066ffe87b23bae5652f5ec00e064
andrew-emil <147343944+andrew-emil@users.noreply.github.com>
2025-05-03 21:57:45 +0300

    added user service config

---
+++ microservices/user-service/src/main/java/com/example/user_service/models/User.java
@@ -0,0 +1,26 @@
+package com.example.user_service.models;
+
+import com.example.user_service.enums.Role;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "role", nullable = false)
+    private Role role;
+
+
+}
